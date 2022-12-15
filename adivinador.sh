bcc59dc004d53b4a57012d774600bad939c060de
ValenAbarzua <valefer119@gmail.com>
2022-12-15 14:40:45 -0300

    Primer commit

--- adivinador.sh
+++ adivinador.sh
@@ -2,25 +2,32 @@
 aleatorio=$(($RANDOM%50+1))
 respuesta=60
 cont=0
-while [$aleatorio -ne $respuesta];
+while [ $aleatorio -ne $respuesta ];
 do
 let cont=cont+1
-if [$cont -gt 10]
+if [ $cont -gt 10 ]
 then
-echo Intentos agotados! Lo sentimos mas suerte para la proxima!
+echo Intentos agotados! Creemos que puedes dar mas, intentalo nuevamente! 
 exit
 else
-read -p "Cual es el numero escondido? 1-50: "respuesta
+read -p  "Cual es el numero escondido? 1-50: " respuesta
 echo Tu numero de intentos $cont de 10
-if [$aleatorio -lt $respuesta] 
+if [ $aleatorio -lt $respuesta ] 
 then
  echo El numero escondido es menor a $respuesta 
-elif [$aleatorio -gt $respuesta]
+elif [ $aleatorio -gt $respuesta ]
 then
  echo El numero escondido es mayor a $respuesta
  fi
 fi
 done
 
-echo Acertaste, el numero es $aleatorio , felicidades
-
+echo "Acertaste, el numero es $aleatorio !!"
+if [ $cont -le 5 ] 
+then
+echo Tenias una brujula de cristal?? Lo adivinaste super rapido, eres lo que conocemos como MASTER
+fi
+if [[ $cont -ge 6  && $cont -lt 10 ]]
+then 
+echo Bien hecho! Fuiste muy buen jugador, aunque no el mejor....
+fi
--- bot.sh
+++ bot.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
-echo " Hola, bienvenido: $USER  "
- 
+opcion=0
+echo "Hola, bienvenido: $USER  "
+sleep 3
+while [ $opcion -ne 3 ];
+do 
+echo Elige una opcion del menu
+echo 1 Ver la hora actual
+echo 2 Ver el clima en Bahia Blanca
+echo 3 Salir del menu
+read opcion
+case $opcion in
+ 1) echo "La hora actual es $(date +%r)" 
+ sleep 3
+ ;;
+ 2) curl wttr.in/Bahia-Blanca?0
+ sleep 3
+ ;;
+ 3) echo Elegiste la opcion de salida, adios!
+ exit
+ ;;
+ *) echo Opcion invalida, lo sentimos! 
+ exit
+ ;;
+esac
+done
 
